8295e977e75a17831295c265add9915319b079b4
ngwatso <ngwatson79@yahoo.com>
2021-09-15 10:57:44 -0400

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ac9aeac21858eca92e1199e665c074ce'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b5a43537-5195-451d-bd56-4a9a5b68c41b'
+};
